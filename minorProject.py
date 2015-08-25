5513e589cc5f7728a3c5486f824de93b260a71d8
sarthakmeh <sarthakmeh03@gmail.com>
2015-08-25 12:14:16 +0530

    testing squshing

--- minorProject.py
+++ minorProject.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 
+#Testing squshing
 #for root,dirs,files in os.walk("/home/sarthakmeh/Desktop/Project"):
     for file in files:
         #Get all the files in root directory ending with .py.
