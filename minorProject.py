4ddced5e74d7cb401e6e6e89bd780d50de7b4200
sarthakmeh <sarthakmeh03@gmail.com>
2015-08-25 12:50:57 +0530

    Bug Fixed . Everything working smooth now

--- minorProject.py
+++ minorProject.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import os
 
+#testing again
 #for root,dirs,files in os.walk("/home/sarthakmeh/Desktop/Project"):
     for file in files:
         #Get all the files in root directory ending with .py.
